8ffbaaa829789ec8786e085171bc6f8848d4a8b0
Vadym Solodovnik <vad.solodovnik@gmail.com>
2023-09-15 14:21:03 +0200

    Remove unused GetToken() method

--- CoinsManagerWebUI/Services/CoinCatalogService.cs
+++ CoinsManagerWebUI/Services/CoinCatalogService.cs
@@ -58,22 +58,5 @@ namespace CoinsManagerWebUI.Services
            
 
         }
-
-        private async Task<string> GetToken()
-        {
-            var clientId = _configuration["Api:ClientId"];
-            var clientSecret = _configuration["Api:ClientSecret"];
-            var authority = _configuration["Api:Authority"];
-            var applicationIdUri = _configuration["Api:ApplicationIdUri"];
-
-            IConfidentialClientApplication app;
-            app = ConfidentialClientApplicationBuilder.Create(clientId)
-                .WithClientSecret(clientSecret)
-                .WithAuthority(authority)
-                .Build();
-
-            var authResult = await app.AcquireTokenForClient(new string[] { $"{applicationIdUri}/.default" }).ExecuteAsync();
-            return authResult.AccessToken;
-        }
     }
 }
